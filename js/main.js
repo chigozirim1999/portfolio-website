5f7a0e91ce0138de63e4a03e64b00c77ba7b7eca
chigozirim1999 <ndinechilex@gmail.com>
2022-09-30 15:03:21 +0100

    slider upload, though not working well yet :(

--- js/main.js
+++ js/main.js
@@ -89,3 +89,20 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+/* Swiper */
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
